bcb02039395f45d91352e5849388fb3673286095
dmitrynovik <cryinstone@gmail.com>
2017-01-02 22:09:26 +1100

    Added unit tests

---
+++ NetEnumerableExtensions.Test/NetEnumerableExtensionsTest.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace NetEnumerableExtensions.Test
+{
+    [TestFixture]
+    public class NetEnumerableExtensionsTest
+    {
+        [Test]
+        public void Add_Many_Adds_All_Items()
+        {
+            var col = CreateEmptyCollection<int>();
+            col.AddMany(new[] { 1, 2, 3 });
+            Assert.AreEqual(3, col.Count);
+        }
+
+        [Test]
+        public void To_HashSet_Has_No_Duplicates()
+        {
+            var set = new[] { 1, 1, 2, 3, 2 }.ToHashSet();
+            Assert.AreEqual(set.Count, set.Distinct().Count());
+        }
+
+        [Test]
+        public void To_SortedSet_Is_Correctly_Sorted()
+        {
+            var set = new[] { 1, 1, 2, 3, 2 }.ToSortedSet();
+            Assert.IsTrue(set.SequenceEqual(new[] { 1, 2, 3 }));
+        }
+
+        [Test]
+        public void Each_Iterator_Over_Null_Is_Safe()
+        {
+            // Does not do anything but does not throw NullReferenceException and hence we don't a null check:
+            CreateNullCollection<int>().Each(Console.WriteLine);
+        }
+
+        [Test]
+        public void Each_Iterator_Over_Collection_Handles_Every_Item()
+        {
+            var collection = new[] {1, 2, 3};
+            var counter = 0;
+
+            collection.Each(x =>
+            {
+                counter++;
+            });
+
+            Assert.AreEqual(3, counter);
+        }
+
+        [Test]
+        public void Filter_Over_Null_Does_Not_Throw_NullReferenceException()
+        {
+            var it = CreateNullCollection<int>().Filter(x => x > 0);
+            Assert.AreEqual(0, it.Count());
+        }
+
+        [Test]
+        public void When_Dictionary_Has_Key_GetOrDefault_Returns_Value()
+        {
+            const int key = 1;
+            const string value = "Some value";
+
+            var dict = new Dictionary<int, string> { [key] = value };
+
+            Assert.AreEqual(value, dict.GetOrDefault(key));
+        }
+
+        [Test]
+        public void When_Dictionary_DoesNot_Contain_Key_GetOrDefault_Returns_Default()
+        {
+            var dict = new Dictionary<int, string>();
+
+            Assert.AreEqual(default(string) /* null */, dict.GetOrDefault(1));
+        }
+
+        [Test]
+        public void MapToMany_With_Input_Of_Size_5_And_Split_Size_2_Produces_3_Series()
+        {
+            var collection = new[] { 1, 2, 3, 4, 5 };
+            var series = collection.MapToMany(3);
+            Assert.AreEqual(2, series.Count());
+        }
+
+        private static ICollection<T> CreateEmptyCollection<T>()
+        {
+            return new List<T>();
+        }
+
+        private static ICollection<T> CreateNullCollection<T>()
+        {
+            return null;
+        }
+    }
+}
---
+++ NetEnumerableExtensions/EnumerableExtensions.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NetEnumerableExtensions
+{
+    public static class EnumerableExtensions
+    {
+        public static bool IsEmpty<T>(this IEnumerable<T> items)
+        {
+            return items == null || !items.Any();
+        }
+
+        public static HashSet<T> ToHashSet<T>(this IEnumerable<T> items)
+        {
+            return items == null ? new HashSet<T>() : new HashSet<T>(items);
+        }
+
+        public static SortedSet<T> ToSortedSet<T>(this IEnumerable<T> items)
+        {
+            return items == null ? new SortedSet<T>() : new SortedSet<T>(items);
+        }
+
+        public static Stack<T> ToStack<T>(this IEnumerable<T> items)
+        {
+            return items == null ? new Stack<T>() : new Stack<T>(items);
+        }
+
+        public static Queue<T> ToQueue<T>(this IEnumerable<T> items)
+        {
+            return items == null ? new Queue<T>() : new Queue<T>(items);
+        }
+
+        public static ConcurrentQueue<T> AsConcurrent<T>(this Queue<T> queue)
+        {
+            return queue == null ? new ConcurrentQueue<T>() : new ConcurrentQueue<T>(queue);
+        }
+
+        public static ConcurrentStack<T> AsConcurrent<T>(this Stack<T> stack)
+        {
+            return stack == null ? new ConcurrentStack<T>() : new ConcurrentStack<T>(stack);
+        }
+
+        public static ConcurrentBag<T> AsConcurrent<T>(this IEnumerable<T> items)
+        {
+            return items == null ? new ConcurrentBag<T>() : new ConcurrentBag<T>(items);
+        }
+
+        public static ConcurrentDictionary<TKey, TVal> AsConcurrent<TKey, TVal>(this IDictionary<TKey, TVal> input)
+        {
+            return input == null ? new ConcurrentDictionary<TKey, TVal>() : new ConcurrentDictionary<TKey, TVal>(input);
+        }
+
+        public static SortedDictionary<TKey, TVal> AsSorted<TKey, TVal>(this IDictionary<TKey, TVal> input)
+        {
+            return input == null ? new SortedDictionary<TKey, TVal>() : new SortedDictionary<TKey, TVal>(input);
+        }
+
+        /// <summary>
+        /// Similar to List.ForEach except it can be used on any iterator. 
+        /// 
+        /// USAGE
+        /// 
+        /// items.Each(x =>
+        /// {
+        ///     Do smth.                   
+        /// });
+        /// </summary>
+        public static void Each<T>(this IEnumerable<T> items, Action<T> action)
+        {
+            if (items != null && action != null)
+                foreach (var i in items)
+                    action(i);
+        }
+
+        /// <summary>Similar to List.AddRange except it can be used on any collection.</summary>
+        public static void AddMany<T>(this ICollection<T> collection, IEnumerable<T> items)
+        {
+            if (collection == null || items == null)
+                return;
+
+            items.Each(collection.Add);
+        }
+
+        /// <summary>NULL-safe selector identical to Enumerable.Where</summary>
+        public static IEnumerable<T> Filter<T>(this IEnumerable<T> collection, Func<T, bool> selector)
+        {
+            if (collection == null)
+                return Enumerable.Empty<T>();
+            if (selector == null)
+                return collection;
+
+            return collection.Where(selector);
+        }
+
+        public static IEnumerable<T> Except<T>(this IEnumerable<T> collection, Func<T, bool> selector)
+        {
+            return Filter(collection, x => !selector(x));
+        }
+
+        /// <summary>
+        /// Returns default value if key not found in the dictionary rather than throwing exception.
+        /// </summary>
+        public static TVal GetOrDefault<TKey, TVal>(this IDictionary<TKey, TVal> dictionary, TKey key)
+        {
+            if (dictionary == null || key == null)
+                return default(TVal);
+
+            return dictionary.ContainsKey(key) ? dictionary[key] : default(TVal);
+        }
+
+        /// <summary>
+        /// Maps a single sequence into the series of sequences limited by size;
+        /// This method is reverse to Enumerable.SelectMany
+        /// </summary>
+        public static IEnumerable<IEnumerable<T>> MapToMany<T>(this ICollection<T> input, int size)
+        {
+            if (size <= 0)
+                throw new ArgumentException($"{nameof(size)} must be greater than 0", nameof(size));
+
+            if (input == null)
+            {
+                yield return Enumerable.Empty<T>();
+                yield break;
+            }
+
+            int offset = 0;
+            while (offset < input.Count)
+            {
+                yield return input.Skip(offset).Take(size);
+                offset += size;
+            }
+        }
+    }
+}
